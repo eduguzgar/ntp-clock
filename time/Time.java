dc2032f6b2fe532b96117e0c22141ae3d969e381
Eduardo Guzman <eduardo.guzman@casa-systems.com>
2023-01-03 15:59:40 +0100

    Add .gitignore

--- time/Time.java
+++ time/Time.java
@@ -4,9 +4,9 @@ public class Time {
     public static byte[] timeSecondsToHMS(long time_seconds)
     {
         byte[] time_hms = new byte[3];
-        time_hms[0] = (byte)(((time_seconds%86400)/3600));
-        time_hms[1] = (byte)((time_seconds%3600)/60);
-        time_hms[2] = (byte)(time_seconds%60);
+        time_hms[0] = (byte)(((time_seconds % 86400) / 3600));
+        time_hms[1] = (byte)((time_seconds % 3600) / 60);
+        time_hms[2] = (byte)(time_seconds % 60);
 
         return time_hms;
     }
